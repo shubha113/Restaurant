1eb0a83cf9e0984074c140ce7ceeb538c1618921
shubha113 <shubhamkumarrai212@gmail.com>
2024-02-13 20:51:21 +0530

    Initial commit

--- Frontend/src/components/Reservation.jsx
+++ Frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "https://restaurant-azure-zeta.vercel.app/api/v1/reservation/send",
+        "http://localhost:4000/api/v1/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
